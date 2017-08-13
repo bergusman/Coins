b6abf47510fcbcd363dc07ed747a40f1efc350f5
Vitaly Berg <bergusman@gmail.com>
2017-08-13 20:06:33 +0300

    Initial Commit

---
+++ Coins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coins
+//
+//  Created by Vitaly Berg on 8/13/17.
+//  Copyright © 2017 Vitaly Berg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
